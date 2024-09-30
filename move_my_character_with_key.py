5cfaf0e238a90ef92e002b1e3b2165733d91c7e9
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2024-09-30 16:08:07 +0900

    과제 고양이 추가 밑 뼈대 작성

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -2,59 +2,31 @@ from pico2d import *
 
 open_canvas()
 
-grass = load_image('grass.png')
-character = load_image('movingCharacter.png')
+ground = load_image('TUK_GROUND.png')
+character = load_image('movingCat.jpg')
 running=True
+def right_event():
+    pass
+def left_event():
+    pass
+def up_event():
+    pass
+def down_event():
+    pass
+def idle_event():
+    pass
+def escaspe_event():
+    pass
 
 def handle_events():
     global running
     global dir
     events = get_events()
-    for event in events:
-        if event.type == SDL_Quit:
-            running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key==SDLK_RIGHT:
-                character.clip_draw(frame * 100, 0, 100, 100, x, 130, 100, 100)
-                dir+=1
-            elif event.key==SDLK_LEFT:
-                character.clip_composite_draw(frame * 100, 0, 100, 100, 0, 'h', x, 130, 200, 200)
-                dir-=1
-            elif event.key==SDLK_ESCAPE:
-                running = False
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT:
-                dir -= 1
-            elif event.key == SDLK_LEFT:
-                dir += 1
+
+def draw_right():
+    character.clip_draw(frame * 160, 4 + 640, 160, 160, x, 10, 100, 100)
 
 
-
-def character_run_right():
-    frame = 0
-    for x in range(0, 800, 10):
-        clear_canvas()
-        grass.draw(400, 30)
-        character.clip_draw(frame * 100, 0, 100, 100, x, 130, 200, 200)
-        update_canvas()
-        handle_events()
-        if not running:
-            break
-        frame = (frame + 1) % 8
-        delay(0.05)
-
-
-def character_run_left():
-    frame = 0
-    for x in range(800, 0, -10):
-        clear_canvas()
-        grass.draw(400, 30)
-        character.clip_composite_draw(frame * 100, 0, 100, 100, 0, 'h', x, 130, 200, 200)
-        update_canvas()
-        if not running:
-            break
-        frame = (frame + 1) % 8
-        delay(0.05)
 
 running=True
 x=800//2
@@ -62,8 +34,8 @@ frame=0
 dir=0
 while running:
     clear_canvas()
-    grass.draw(400, 30)
-    character.clip_draw(frame * 100, 150, 100, 100, x, 130, 100, 100)
+    ground.draw(400,300,800,600)
+    draw_right()
     update_canvas()
     handle_events()
     frame = (dir + 1) % 8
